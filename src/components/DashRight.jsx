64338f46e2b42ba9377e3a593e1583a140edec73
ervanervan <ervanervan040@gmail.com>
2024-02-18 14:22:43 +0700

    update dash center and right

--- src/components/DashRight.jsx
+++ src/components/DashRight.jsx
@@ -17,44 +17,40 @@ const DashRight = () => {
           stiffness: 200,
         },
       }}
-      className="lg:block w-ful lg:w-fit hidden  max-xl:hidden "
+      className="w-full"
     >
-      <div className=" md:w-60 w-full rounded-2xl h-fit sticky top-5 ">
+      <div className="md:w-60 w-full md:rounded-xl h-fit sticky top-5 ">
         <div>
-          <div className="bg-[#1C1C1C] min-w-min rounded-2xl p-4 border border-neutral-800 h-fit">
-            <h2 className="font-RubikBold text-neutral-200">
-              90+ Framer Shadows
-            </h2>
-            <p className="text-xs my-3 text-neutral-400 font-RubikRegular">
+          <div className="bg-[#141414] md:bg-[#1d1d1d] min-w-min rounded-2xl p-4 md:border border-neutral-800 h-fit">
+            <h2 className="font-bold text-neutral-200">90+ Framer Shadows</h2>
+            <p className="text-xs my-3 text-neutral-400 font-normal">
               Stop worrying about perfecting <br /> the shadows, just Copy &
               Paste <br />
               from the 80+ Shadows collection
             </p>
-            <button className="bg-[#696969] h-7 p-1 rounded-md text-xs w-full font-RubikMedium text-neutral-50 ">
+            <button className="bg-[#696969] h-7 p-1 rounded-md text-xs w-full font-medium text-neutral-50 ">
               Download
             </button>
 
             <div className="border border-neutral-700 my-5" />
 
             <div className="text-neutral-400">
-              <h1 className="font-RubikMedium text-neutral-200">Featured in</h1>
-              <div className="flex items-center gap-x-3 mt-6 hover:bg-neutral-800  p-2 rounded-md ">
+              <h1 className="font-medium text-neutral-200">Featured in</h1>
+              <div className="flex items-center gap-x-3 mt-6 hover:bg-neutral-800 p-2 rounded-md">
                 <div className="bg-neutral-700/50 h-8 w-8 rounded-full flex items-center justify-center">
                   <Github color="white" width={24} height={24} />
                 </div>
-                <h3 className="text-xs ">
-                  9 Essential Javascript <br /> Functions
-                </h3>
+                <h3 className="text-xs">9 Essential Javascript Functions</h3>
               </div>
-              <div className="flex items-center gap-x-3 mt-6 hover:bg-neutral-800  p-2 rounded-md">
+              <div className="flex items-center gap-x-3 mt-6 hover:bg-neutral-800 p-2 rounded-md">
                 <div className="bg-neutral-700/50 h-8 w-8 rounded-full flex items-center justify-center">
                   <Figma color="white" width={24} height={24} />
                 </div>
                 <h3 className="text-xs ">
-                  How to easily creat React <br /> animations: Figma
+                  How to easily creat React animations: Figma
                 </h3>
               </div>
-              <div className="flex items-center gap-x-3 mt-6 hover:bg-neutral-800  p-2 rounded-md">
+              <div className="flex items-center gap-x-3 mt-6 hover:bg-neutral-800 p-2 rounded-md">
                 <div className="bg-neutral-700/50 h-8 w-8 rounded-full flex items-center justify-center">
                   <Css3 color="white" width={24} height={24} />
                 </div>
@@ -75,11 +71,11 @@ const DashRight = () => {
             width={1000}
             height={1000}
             className="h-32 w-56 object-cover rounded-lg"
-            src="/game.jpg"
+            src="/logo.png"
             alt=""
           />
-          <p className="my-3 font-RubikMedium text-sm">3D Games in Reactjs</p>
-          <button className="bg-[#696969] h-7 p-1 rounded-md text-xs w-full font-RubikMedium ">
+          <p className="my-3 font-medium text-sm">3D Games in Reactjs</p>
+          <button className="bg-[#696969] h-7 p-1 rounded-md text-xs w-full font-medium">
             Get Now @20% OFF
           </button>
         </div>
