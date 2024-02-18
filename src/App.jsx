de631ee7f0c67fe6154a0f8aa7adcb6a51b08026
ervanervan <ervanervan040@gmail.com>
2024-02-18 11:17:43 +0700

    finished home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-import "./App.css";
+import "./index.css";
+import { Link } from "react-router-dom";
+import { Home, Github } from "iconoir-react";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <header className="h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center justify-center mx-auto px-8 md:px-0 lg:max-w-2xl">
+        <div className="border-2 rounded-full">
+          <img
+            src="/ervan.jpg"
+            alt=""
+            className="object-cover w-36 h-36 rounded-full"
+          />
+        </div>
+        <h1 className="font-extrabold text-4xl md:text-5xl lg:text-6xl text-center my-9 btn-shine">
+          Ervan Kurniawan
+        </h1>
+
+        <p className="text-zinc-400 text-center font-normal">
+          I have interest and ability in programming, especially in HTML, CSS,
+          JavaScript, ReactJS, and TailwindCSS programming languages.
+        </p>
+
+        <div className="flex justify-center">
+          <div className="fixed bottom-8 flex gap-x-5 text-neutral-500">
+            <Link href={"/dashboard"}>
+              <span>
+                <Home color="gray" height={24} width={24} />
+              </span>
+            </Link>
+            {/* <span>
+                <Github color="gray" height={24} width={24} />
+              </span> */}
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,51 @@ export default {
 
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        gradient:
+          "linear-gradient(145.37deg, rgba(255, 255, 255, 0.09) -8.75%, rgba(255, 255, 255, 0.027) 83.95%)",
+        gradientHover:
+          "linear-gradient(145.37deg, rgba(255, 255, 255, 0.1) -8.75%, rgba(255, 255, 255, 0.057) 83.95%)",
+        shine:
+          "linear-gradient(45deg, rgba(255,255,255,0) 45%,rgba(255,255,255,1) 50%,rgba(255,255,255,0) 55%,rgba(255,255,255,0) 100%)",
+      },
+
+      animation: {
+        blob: "blob 7s infinite",
+      },
+
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
+        shinee: {
+          "0%": { backgroundPosition: "-80%" },
+          "100%": { backgroundPosition: "80%" },
+        },
+        dash: {
+          "0%": { strokeDashoffset: 1000 },
+          "100%": { strokeDashoffset: 0 },
+        },
+      },
+    },
   },
+
+  fontFamily: {
+    "rubik": ["Rubik", "sans - serif"],
+  },
+
   plugins: [],
 }
 
