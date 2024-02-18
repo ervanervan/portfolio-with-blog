20f34c556005201a29175eff21c2e6aa23a3935f
ervanervan <ervanervan040@gmail.com>
2024-02-18 11:31:15 +0700

    finished update and add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,14 @@
 import "./index.css";
-import { Link } from "react-router-dom";
-import { Home, Github } from "iconoir-react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
-    <header className="h-screen flex items-center justify-center">
-      <div className="flex flex-col items-center justify-center mx-auto px-8 md:px-0 lg:max-w-2xl">
-        <div className="border-2 rounded-full">
-          <img
-            src="/ervan.jpg"
-            alt=""
-            className="object-cover w-36 h-36 rounded-full"
-          />
-        </div>
-        <h1 className="font-extrabold text-4xl md:text-5xl lg:text-6xl text-center my-9 btn-shine">
-          Ervan Kurniawan
-        </h1>
-
-        <p className="text-zinc-400 text-center font-normal">
-          I have interest and ability in programming, especially in HTML, CSS,
-          JavaScript, ReactJS, and TailwindCSS programming languages.
-        </p>
-
-        <div className="flex justify-center">
-          <div className="fixed bottom-8 flex gap-x-5 text-neutral-500">
-            <Link href={"/dashboard"}>
-              <span>
-                <Home color="gray" height={24} width={24} />
-              </span>
-            </Link>
-            {/* <span>
-                <Github color="gray" height={24} width={24} />
-              </span> */}
-          </div>
-        </div>
-      </div>
-    </header>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/dashboard" element={<DashboardPage />}></Route>
+    </Routes>
   );
 }
 
