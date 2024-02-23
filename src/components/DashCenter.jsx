bad78cd7e82aad2296c47d605e62de6a949bbc6b
ervanervan <ervanervan040@gmail.com>
2024-02-23 17:39:27 +0700

    update and add content : dash center

--- src/components/DashCenter.jsx
+++ src/components/DashCenter.jsx
@@ -40,7 +40,7 @@ const DashCenter = () => {
 
           <div className="border border-neutral-700 my-5" />
 
-          <div className="  bg-[#1C1C1C] rounded-lg text-neutral-400">
+          <div className="bg-[#1C1C1C] rounded-lg text-neutral-400">
             <Link>
               <div className="hover:bg-neutral-800 duration-200 transition-all ease-in p-4">
                 <div className="flex items-center gap-x-3">
@@ -63,7 +63,126 @@ const DashCenter = () => {
                         TailwindCSS
                       </p>
                     </div>
-                    <span className="text-xs">Sunday, October 22, 2023</span>
+                    <span className="text-xs">Monday, December 25, 2023</span>
+                  </div>
+                </div>
+              </div>
+            </Link>
+            <Link>
+              <div className="hover:bg-neutral-800 duration-200 transition-all ease-in p-4">
+                <div className="flex items-center gap-x-3">
+                  <img
+                    height={1000}
+                    width={1000}
+                    className="size-20 object-cover rounded-md"
+                    src="/logo.png"
+                    alt=""
+                  />
+                  <div className="flex flex-col gap-2">
+                    {/* <span className="text-sm">July 22, 2023</span> */}
+                    <h2 className="text-base font-medium">Anime Movies</h2>
+                    {/* <p className="text-sm font-medium">Payna Finance</p> */}
+                    <div className="flex items-center gap-x-2 font-semibold">
+                      <p className="bg-[#282828] w-fit text-neutral-300 rounded-md px-2 h-5 flex items-center justify-center text-[11px] ">
+                        TypeScript
+                      </p>
+                      <p className="bg-[#282828] w-fit text-neutral-300 rounded-md px-2 h-5 flex items-center justify-center text-[11px] ">
+                        TailwindCSS
+                      </p>
+                    </div>
+                    <span className="text-xs">Sunday, December 17, 2023</span>
+                  </div>
+                </div>
+              </div>
+            </Link>
+            <Link>
+              <div className="hover:bg-neutral-800 duration-200 transition-all ease-in p-4">
+                <div className="flex items-center gap-x-3">
+                  <img
+                    height={1000}
+                    width={1000}
+                    className="size-20 object-cover rounded-md"
+                    src="/ervan.jpg"
+                    alt=""
+                  />
+                  <div className="flex flex-col gap-2">
+                    {/* <span className="text-sm">July 22, 2023</span> */}
+                    <h2 className="text-base font-medium">Disney Clone</h2>
+                    {/* <p className="text-sm font-medium">Payna Finance</p> */}
+                    <div className="flex items-center gap-x-2 font-semibold">
+                      <p className="bg-[#282828] w-fit text-neutral-300 rounded-md px-2 h-5 flex items-center justify-center text-[11px] ">
+                        ReactJS
+                      </p>
+                      <p className="bg-[#282828] w-fit text-neutral-300 rounded-md px-2 h-5 flex items-center justify-center text-[11px] ">
+                        TailwindCSS
+                      </p>
+                    </div>
+                    <span className="text-xs">Sunday, December 31, 2023</span>
+                  </div>
+                </div>
+              </div>
+            </Link>
+          </div>
+
+          {/* without image */}
+
+          <div className="mt-6 bg-[#1C1C1C] rounded-lg text-neutral-400">
+            <div className="hover:bg-neutral-800 duration-200 transition-all ease-in p-4">
+              <div className="flex items-center gap-x-3">
+                <div>
+                  <span className="text-sm">July 22, 2023</span>
+                  <h2 className="text-sm font-medium">
+                    Smooth Animation with React and Framer Motion
+                  </h2>
+                </div>
+              </div>
+            </div>
+            <div className="hover:bg-neutral-800 duration-200 transition-all ease-in p-4">
+              <div className="flex items-center gap-x-3">
+                <div>
+                  <span className="text-sm">July 22, 2023</span>
+                  <h2 className="text-sm font-medium">
+                    Smooth Animation with React and Framer Motion
+                  </h2>
+                </div>
+              </div>
+            </div>
+            <div className="hover:bg-neutral-800 duration-200 transition-all ease-in p-4">
+              <div className="flex items-center gap-x-3">
+                <div>
+                  <span className="text-sm">July 22, 2023</span>
+                  <h2 className="text-sm font-medium">
+                    Smooth Animation with React and Framer Motion
+                  </h2>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-6 bg-[#1C1C1C] rounded-lg text-neutral-400">
+            <Link>
+              <div className="hover:bg-neutral-800 duration-200 transition-all ease-in p-4">
+                <div className="flex items-center gap-x-3">
+                  <img
+                    width={1000}
+                    height={1000}
+                    className="size-20 object-cover rounded-md"
+                    src="/ervan.jpg"
+                    alt=""
+                  />
+                  <div className="flex flex-col gap-2">
+                    {/* <span className="text-sm">July 22, 2023</span> */}
+                    <h2 className="text-base font-medium">Weserve</h2>
+                    {/* <p className="text-sm font-medium">Payna Finance</p> */}
+                    <div className="flex items-center gap-x-2 font-semibold">
+                      <p className="bg-[#282828] w-fit text-neutral-300 rounded-md px-2 h-5 flex items-center justify-center text-[11px] ">
+                        TypeScript
+                      </p>
+                      <p className="bg-[#282828] w-fit text-neutral-300 rounded-md px-2 h-5 flex items-center justify-center text-[11px] ">
+                        TailwindCSS
+                      </p>
+                    </div>
+                    <span className="text-xs">Monday, December 25, 2023</span>
                   </div>
                 </div>
               </div>
