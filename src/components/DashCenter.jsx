c1e323f0b653a988163cb29a8070eda18a82129a
ervanervan <ervanervan040@gmail.com>
2024-03-09 19:08:59 +0700

    update dash center

--- src/components/DashCenter.jsx
+++ src/components/DashCenter.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { MediaImageList } from "iconoir-react";
-import { motion, useAnimation } from "framer-motion";
+import { motion } from "framer-motion";
 
 const DashCenter = () => {
   return (
