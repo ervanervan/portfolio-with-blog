95d39ba5d2a4d4f5902d30223b90a9203ffffff5
ervanervan <ervanervan040@gmail.com>
2024-03-19 10:04:16 +0700

    update: content blog

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,17 +1,34 @@
 import React from "react";
+import { ArrowLeftCircle } from "iconoir-react";
+import { Link } from "react-router-dom";
 
 const BlogPost = ({ title, description, content, imageUrl }) => {
+  const paragraphs = content.split("\n");
   return (
-    <div className="bg-white shadow-lg rounded-lg overflow-hidden mb-8">
-      <div className="p-6">
-        <h2 className="text-2xl font-bold mb-4">{title}</h2>
-        <p className="text-gray-700 mb-4">{description}</p>
-        {imageUrl && (
-          <img src={imageUrl} alt={title} className="mb-4 rounded-lg" />
-        )}
-        <div className="prose">{content}</div>
+    <>
+      <div className="max-w-xl mx-auto bg-[#1C1C1C] rounded-lg text-neutral-400">
+        <div className="p-6">
+          <Link
+            to={"/dashboard"}
+            className="flex items-center gap-x-2 py-2 mb-2 w-fit rounded-lg"
+          >
+            <ArrowLeftCircle width={20} height={20} />
+            <span className="text-sm">back to dashboard</span>
+          </Link>
+          <h2 className="text-2xl font-bold mb-4">{title}</h2>
+          <p className="text-neutral-400 mb-4">{description}</p>
+          <div className="border border-neutral-700 my-5" />
+          {paragraphs.map((paragraph, index) => (
+            <p key={index} className="mb-4">
+              {paragraph}
+            </p>
+          ))}
+          {imageUrl && (
+            <img src={imageUrl} alt={title} className="mb-4 rounded-lg" />
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -7,8 +7,15 @@ const BlogPage = () => {
       <BlogPost
         title="Easy Steps to Push Your Project to GitHub with Git: Guide for Developers"
         description="Let's learn the simple way to push your project to GitHub! In this blog, we'll discuss the steps in a relaxed and understandable style. No need to worry about technicalities anymore because we'll be there to guide you from start to finish. Ready to embark on your journey towards smoother and more productive collaboration?"
-        content="Konten blog pertama Anda di sini. Anda dapat menambahkan teks, gambar, video, dll."
-        imageUrl="https://example.com/image1.jpg"
+        content={`Hey, fellow developers! Pushing projects to GitHub can often be confusing. But don't worry, because you're in the right place to get help!
+
+        In this blog, we'll dive deep into how to push projects using Git. We'll discuss it in simple and friendly language, so no need to fret about technical jargon!
+        
+        From setting up repositories, adding changes, to resolving merge conflicts, we'll cover it all directly and enjoyably. So, get ready to gain newfound confidence in managing your projects on GitHub!
+        
+        Come on, let's explore a more enjoyable and productive world of collaboration together. We can't wait to share this knowledge with you and see how your projects flourish rapidly!
+
+        `}
       />
     </div>
   );
