ed1f7e3e25e8c16292c5b1d184c3cdd6b49bb4c9
ervanervan <ervanervan040@gmail.com>
2024-03-19 09:28:04 +0700

    update: blog blog page

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import BlogPost from "../components/BlogPost";
 
 const BlogPage = () => {
-  return <div>BlogPage</div>;
+  return (
+    <div className="container mx-auto mt-8">
+      <BlogPost
+        title="Easy Steps to Push Your Project to GitHub with Git: Guide for Developers"
+        description="Let's learn the simple way to push your project to GitHub! In this blog, we'll discuss the steps in a relaxed and understandable style. No need to worry about technicalities anymore because we'll be there to guide you from start to finish. Ready to embark on your journey towards smoother and more productive collaboration?"
+        content="Konten blog pertama Anda di sini. Anda dapat menambahkan teks, gambar, video, dll."
+        imageUrl="https://example.com/image1.jpg"
+      />
+    </div>
+  );
 };
 
 export default BlogPage;
